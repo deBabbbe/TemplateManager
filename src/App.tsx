09ed4291d77f4aa155eec9e1ffca2cb1af521e2d
deBabbbe <neotbh@gmx.de>
2022-02-12 22:39:24 +0100

    Added richt text editor

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,28 @@
 import "./App.css";
 import Categories from "./components/Categories";
+import RichTextEditor from "react-rte";
+import { useState } from "react";
+import {
+  Accordion,
+  AccordionDetails,
+  AccordionSummary,
+  Typography,
+} from "@mui/material";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 
 function App() {
+  const [value, setValue] = useState(RichTextEditor.createEmptyValue());
+
   return (
     <>
       <div className="row">
         <div className="column-left">
           <Categories />
         </div>
-        <div className="column"></div>
-        <div className=".column-right"></div>
+        <div className="column-mid">
+          <RichTextEditor value={value}></RichTextEditor>
+        </div>
+        <div className=".column-right">Placeholder</div>
       </div>
     </>
   );
