3cf2bf1a4bce9b7a8693038d89202bdf6a0d4fad
deBabbbe <neotbh@gmx.de>
2022-02-12 20:59:30 +0100

    Added add button

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
-import { useState } from 'react'
-import './App.css'
-import Tree from '@naisutech/react-tree'
+import "./App.css";
+import Add from "@mui/icons-material/Add";
+import Tree from "@naisutech/react-tree";
+import { useState } from "react";
 
 function App() {
   const entries = [
@@ -20,18 +21,26 @@ function App() {
       parentId: "AutoResponse",
     },
     {
-      label: "Anfrage", id: "Anfrage", parentId: null
+      label: "Anfrage",
+      id: "Anfrage",
+      parentId: null,
     },
     {
-      label: "Auftrag", id: "Auftrag", parentId: null
+      label: "Auftrag",
+      id: "Auftrag",
+      parentId: null,
     },
     {
-      label: "Beschwerde", id: "Beschwerde", parentId: null
+      label: "Beschwerde",
+      id: "Beschwerde",
+      parentId: null,
     },
     {
-      label: "Kündigung", id: "Kündigung", parentId: null
-    }
-  ]
+      label: "Kündigung",
+      id: "Kündigung",
+      parentId: null,
+    },
+  ];
 
   const [ent, setEnt] = useState(entries);
   const [entryValue, setEntryValue] = useState("");
@@ -39,19 +48,32 @@ function App() {
   const handleKeyDown = (event: any) => {
     event.persist();
     if (event.key === "Enter") {
-      setEntryValue("");
-      console.warn(event.target.value)
-      setEnt([...ent, {
-        label: event.target.value, id: event.target.value, parentId: null
-      }])
+      addEntry(event.target.value);
     }
   };
 
   return (
     <>
-      <input value={entryValue} onChange={(e) => setEntryValue(e.target.value)} onKeyDown={handleKeyDown}></input>
-      <Tree animations={true} nodes={ent}></Tree>
+      <input
+        value={entryValue}
+        onChange={(e) => setEntryValue(e.target.value)}
+        onKeyDown={handleKeyDown}
+      ></input>
+      <Add onClick={() => addEntry(entryValue)}>Hinzufügen</Add>
+      <Tree noIcons={true} animations={true} nodes={ent}></Tree>
     </>
-  )
+  );
+
+  function addEntry(value: string) {
+    setEntryValue("");
+    setEnt([
+      ...ent,
+      {
+        label: value,
+        id: value,
+        parentId: null,
+      },
+    ]);
+  }
 }
-export default App
+export default App;
